5b226ce95ea453ab1d154fa6a424c01261791a83
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-04 22:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
-
+let list=document.getElementById('infi-list');
+function addelements()
+{
+	for(let i=0;i<2;i++)
+		{
+			const listitem=document.createElement('li');
+			listitem.textContent=`Item ${list.children.length+1}`;
+			list.appendChild(listitem);
+}
+}
+for(let i=0;i<10;i++)
+				{
+				addelements();	
+				}
+function checkscroll()
+{
+	if(window.innerHeight+window.scrollY>=document.body.scrollHeight)
+	{
+		addelements();
+	}
+}
+window.addEventListener("scroll",checkscroll);
